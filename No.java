fb216e67bfb1bb2e942c84e539d0dc519231baa1
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-11-03 13:27:24 -0300

    feat: os construtores de um nó

--- No.java
+++ No.java
@@ -1,5 +1,23 @@
 public class No {
+    private No noAnterior;
     private String dado;
     private No proximoNo;
-    private No noAnterior;
+
+    public No() { /* Um nó vazio */
+        this.noAnterior = null;
+        this.dado = null;
+        this.proximoNo = null;
+    }
+
+    public No(String dado) {
+        this.noAnterior = null;
+        this.dado = dado;
+        this.proximoNo = null;
+    }
+
+    public No(No noAnterior, String dado, No proximoNo) {
+        this.noAnterior = noAnterior;
+        this.dado = dado;
+        this.proximoNo = proximoNo;
+    }
 }
