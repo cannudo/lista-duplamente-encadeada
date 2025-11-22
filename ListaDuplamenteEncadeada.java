87e9678af0f234274de3f987c79c761a5cf0b391
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-11-22 20:40:05 -0300

    Saí da linha nesse commit. Mas os inserir sequencialmente estão OK aparentemente.

---
+++ ListaDuplamenteEncadeada.java
@@ -0,0 +1,97 @@
+public class ListaDuplamenteEncadeada {
+    No primeiroNo;
+    No ultimoNo;
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        No noA = new No("B");
+        No noB = new No("C");
+        No noX = new No("X");
+        ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
+        lista.inserirEmSequencia(noA);
+        System.out.println("A lista só tem um nó? " + lista.soTemUmNo());
+        lista.inserirEmSequencia(noB);
+        lista.inserirEmSequencia(noX);
+        System.out.println("A lista só tem um nó? " + lista.soTemUmNo());
+    }
+
+    public ListaDuplamenteEncadeada() {
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+    }
+
+    public boolean estaVazia() {
+        return (this.primeiroNo == null) && (this.ultimoNo == null);
+    }
+
+    public boolean temPrimeiroNo() {
+        return this.primeiroNo != null;
+    }
+
+    public boolean temUltimoNo() {
+        return this.ultimoNo != null;
+    }
+
+    public boolean soTemUmNo() {
+        return this.primeiroNo == this.ultimoNo;
+    }
+
+    public void inserirDepois(No noDaVez, No proximoNo) {
+        No p = noDaVez;
+        No noOrfao = p.getProximoNo();
+        No X = proximoNo;
+        proximoNo.setDado("X");
+        proximoNo.setNoAnterior(noDaVez);
+        proximoNo.setProximoNo(noOrfao);
+    }
+
+    public void setPrimeiroNo(No primeiroNo) {
+        this.primeiroNo = primeiroNo;
+    }
+
+    public void inserirEmSequencia(No novoNo) {
+        // 1 - A lista está vazia
+        if(this.estaVazia()) {
+            this.setPrimeiroNo(novoNo);
+            this.setUltimoNo(novoNo);
+        } else {
+            // 2 - Tem um nó na lista, mas só ele
+            if(this.soTemUmNo()) {
+                this.setUltimoNo(novoNo);
+                this.getPrimeiroNo().setProximoNo(novoNo);
+                this.getUltimoNo().setNoAnterior(this.getPrimeiroNo());
+            } else {
+                // 3 - o nó é só mais um
+                novoNo.setNoAnterior(this.getUltimoNo());
+                this.getUltimoNo().setProximoNo(novoNo);
+                this.setUltimoNo(novoNo);
+            }
+        }
+    }
+
+    public No getUltimoNo() {
+        return ultimoNo;
+    }
+
+    public void setUltimoNo(No ultimoNo) {
+        this.ultimoNo = ultimoNo;
+    }
+
+    public No getPrimeiroNo() {
+        return this.primeiroNo;
+    }
+
+    public int tamanho() {
+        if(this.estaVazia()) {
+            return 0;
+        }
+        int contador = 0;
+        No ultimoNo = this.ultimoNo;
+        No oNoDaVez = this.primeiroNo;
+        while(oNoDaVez != ultimoNo) {
+            contador++;
+            oNoDaVez = oNoDaVez.getProximoNo();
+        }
+        return contador;
+    }
+}
